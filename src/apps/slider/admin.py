35d918ab34c333cc8be87d8723545ba04e7249db
kvitee <kvitee41@gmail.com>
2024-11-19 16:42:14 +0800

    Create models in 'slider' app and register them.

--- src/apps/slider/admin.py
+++ src/apps/slider/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 
+from apps.slider.models import Slider, SliderEntry
 
-# Register your models here.
+
+@admin.register(Slider)
+class SliderAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SliderEntry)
+class SliderEntryAdmin(admin.ModelAdmin):
+    pass
--- src/apps/slider/models.py
+++ src/apps/slider/models.py
@@ -1,4 +1,27 @@
 from django.db import models
 
+from filer.fields.image import FilerImageField
 
-# Create your models here.
+
+class Slider(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = "sliders"
+
+
+class SliderEntry(models.Model):
+    slider = models.ForeignKey(
+        to=Slider,
+        on_delete=models.CASCADE,
+    )
+
+    number = models.PositiveSmallIntegerField()
+    image = FilerImageField(
+        related_name="entry_image",
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        db_table = "slider_entries"
+        ordering = ["number"]
