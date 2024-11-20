2c26759a4e9c42eb160cc2ed3bc5fc4b2117d1f4
kvitee <kvitee41@gmail.com>
2024-11-20 17:29:25 +0800

    Update models in slider app.
    - Add slug field to Slider model;
    - Add verbose names for fields and tables;
    - Override __str__ functions.

--- src/apps/slider/models.py
+++ src/apps/slider/models.py
@@ -4,24 +4,54 @@ from filer.fields.image import FilerImageField
 
 
 class Slider(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(
+        verbose_name="название",
+        max_length=255,
+    )
+
+    slug = models.SlugField(
+        verbose_name="короткое уникальное название (slug)",
+        max_length=255,
+        unique=True,
+        default="start_page_slider",
+    )
+
+    def __str__(self):
+        return str(self.name)
 
     class Meta:
         db_table = "sliders"
+        verbose_name = "слайдер"
+        verbose_name_plural = "слайдеры"
 
 
 class SliderEntry(models.Model):
     slider = models.ForeignKey(
         to=Slider,
         on_delete=models.CASCADE,
+        related_name="entry_slider",
+        verbose_name = "слайдер",
     )
 
-    number = models.PositiveSmallIntegerField()
-    image = FilerImageField(
-        related_name="entry_image",
-        on_delete=models.CASCADE,
+    number = models.PositiveSmallIntegerField(
+        verbose_name="порядок",
+        default=0,
     )
+
+    image = FilerImageField(
+        on_delete=models.CASCADE,
+        related_name="entry_image",
+        verbose_name="картинка"
+    )
+
+    def __str__(self):
+        return "Элемент слайдера \"{}\" с картинкой {}".format(
+            self.slider,
+            self.image,
+        )
 
     class Meta:
         db_table = "slider_entries"
+        verbose_name = "элемент слайдера"
+        verbose_name_plural = "элементы слайдеров"
         ordering = ["number"]
